3b582ec6cc9fd8ca969b5d65ca30f4a8545cb1ca
Rodrigo Vieira <rodrigo@slayer.tech>
2023-04-05 19:36:24 -0300

    💄 Prepare support to code highlight in dark theme

--- src/components/DPageSourceCode.vue
+++ src/components/DPageSourceCode.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .source-code
-  .code.white
+  .code(:class="coloring")
     .lines(v-if="lines")
       template(v-for="(line, index) in lines" :key="index")
         a.line(:href="`${$store.state.page.base}#${anchor}${line}`" :id="`${anchor}${line}`")
@@ -12,8 +12,9 @@
 
 <script>
 import Prism from 'prismjs'
-
-import 'prismjs/components/prism-markup-templating'
+// @ Load Prism languages
+import 'prismjs/components/prism-markup-templating' // dependency for prism-php extension
+// PHP
 import 'prismjs/components/prism-php'
 
 export default {
@@ -35,6 +36,10 @@ export default {
   },
 
   computed: {
+    coloring () {
+      return this.$q.dark.isActive ? 'dark' : 'white'
+    },
+
     anchor () {
       return this.printToLetter(this.index + 1)
     },
@@ -119,6 +124,7 @@ export default {
           margin-top: 3px
           margin-right: 5px
           visibility: hidden
+
     pre
       display: flex
 
@@ -140,63 +146,127 @@ export default {
         min-width: 100%
         padding: 0
 
-/* Tokens */
-.token.comment,
-.token.prolog,
-.token.doctype,
-.token.cdata
-  color: #4e5a65
+    &.white
+      .token.comment,
+      .token.prolog,
+      .token.doctype,
+      .token.cdata
+        color: #4e5a65
 
-.token.punctuation
-  color: #999
+      .token.punctuation
+        color: #999
 
-.token.namespace
-  opacity: .7
+      .token.namespace
+        opacity: .7
 
-.token.property,
-.token.tag,
-.token.boolean,
-.token.number,
-.token.constant,
-.token.symbol,
-.token.deleted
-  color: #905
+      .token.property,
+      .token.tag,
+      .token.boolean,
+      .token.number,
+      .token.constant,
+      .token.symbol,
+      .token.deleted
+        color: #905
 
-.token.selector,
-.token.attr-name,
-.token.string,
-.token.char,
-.token.builtin,
-.token.inserted
-  color: #416200
+      .token.selector,
+      .token.attr-name,
+      .token.string,
+      .token.char,
+      .token.builtin,
+      .token.inserted
+        color: #416200
 
-.token.operator,
-.token.entity,
-.token.url,
-.language-css .token.string,
-.style .token.string
-  color: #9a6e3a
+      .token.operator,
+      .token.entity,
+      .token.url,
+      .language-css .token.string,
+      .style .token.string
+        color: #9a6e3a
 
-.token.atrule,
-.token.attr-value,
-.token.keyword
-  color: #07a
+      .token.atrule,
+      .token.attr-value,
+      .token.keyword
+        color: #07a
 
-.token.function,
-.token.class-name
-  color: #DD4A68
+      .token.function,
+      .token.class-name
+        color: #DD4A68
 
-.token.regex,
-.token.important,
-.token.variable
-  color: #e90
+      .token.regex,
+      .token.important,
+      .token.variable
+        color: #e90
 
-.token.important,
-.token.bold
-  font-weight: bold
-.token.italic
-  font-style: italic
+      .token.important,
+      .token.bold
+        font-weight: bold
+      .token.italic
+        font-style: italic
 
-.token.entity
-  cursor: help
+      .token.entity
+        cursor: help
+    &.dark
+      // TODO Andromeda Colorized
+      .token.comment,
+      .token.prolog,
+      .token.doctype,
+      .token.cdata
+        color: #A0A1A7cc
+
+      .token.punctuation
+        color: #D5CED9
+
+      .token.namespace
+        opacity: .7
+
+      .token.property
+        color: #7CB7FF // Blue
+      .token.constant.boolean
+        color: #f39c12 // Orange
+      .token.number
+        color: #f39c12 // Orange
+      .token.constant
+        color: #ff68bc
+      .token.tag,
+      .token.symbol,
+      .token.deleted
+        color: #905
+
+      .token.selector,
+      .token.attr-name,
+      .token.string,
+      .token.char,
+      .token.builtin,
+      .token.inserted
+        color: #96E072
+
+      .token.operator,
+      .token.entity,
+      .token.url,
+      .language-css .token.string,
+      .style .token.string
+        color: #9a6e3a
+
+      .token.atrule,
+      .token.attr-value,
+      .token.keyword
+        color: #c74ded // Purple
+
+      .token.function,
+      .token.class-name
+        color: #FFE66D
+
+      .token.regex,
+      .token.important,
+      .token.variable
+        color: #7CB7FF // Blue
+
+      .token.important,
+      .token.bold
+        font-weight: bold
+      .token.italic
+        font-style: italic
+
+      .token.entity
+        cursor: help
 </style>
