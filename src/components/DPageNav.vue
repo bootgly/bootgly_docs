6ba5cc3f73f6b41e11795fd0bf7ba8ab6ec25a1c
Rodrigo Vieira <rodrigo@slayer.tech>
2023-04-15 06:02:34 -0300

    🐛 Fix routes slice in DPageNav + links margin

--- src/components/DPageNav.vue
+++ src/components/DPageNav.vue
@@ -19,7 +19,7 @@ export default {
   computed: {
     prev () {
       const base = this.$store.state.page.base
-      const routes = this.$router.options.routes.slice(0, -1)
+      const routes = this.$router.options.routes.slice(0, -2)
 
       for (let i = 0; i < routes.length; i++) {
         if ('/' + base === routes[i].path) {
@@ -33,7 +33,7 @@ export default {
     },
     next () {
       const base = this.$store.state.page.base
-      const routes = this.$router.options.routes.slice(0, -1)
+      const routes = this.$router.options.routes.slice(0, -2)
 
       for (let i = 0; i < routes.length; i++) {
         if ('/' + base === routes[i].path) {
@@ -51,13 +51,15 @@ export default {
 
 <style lang="sass" scoped>
 nav
+  display: inline-block
+  width: 100%
   min-height: 36px
-  padding-top: 15px
   margin-top: 15px
   border-top: 3px solid #e0e0e0
   &:first-child
     margin-top: calc(100vh - 200px)
   .link
+    margin-top: 20px
     border: 1px solid #e0e0e0
     padding: 20px !important
 </style>
