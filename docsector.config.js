ed1d80914a6108e88470791163ab9c23752de509
Rodrigo Vieira <rodrigo.vieira92@outlook.com>
2026-08-22 22:36:33 -0300

    Bump framework version to v1.0.0-beta.2

--- docsector.config.js
+++ docsector.config.js
@@ -8,11 +8,15 @@ export default {
   branding: {
     logo: 'https://docs.bootgly.com/images/logo/bootgly-logo.webp',
     name: 'Bootgly',
-    version: 'v1.0.0-beta.1',
+    version: 'v1.0.0-beta.2',
     versions: [
       {
-        id: 'v1.0.0-beta.1',
+        id: 'v1.0.0-beta.2',
         current: true,
+        released: true
+      },
+      {
+        id: 'v1.0.0-beta.1',
         released: true
       },
       {
