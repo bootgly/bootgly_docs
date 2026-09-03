4a54135f7a4fec251c4af951a53f20424469677c
Rodrigo Vieira <rodrigo.vieira92@outlook.com>
2026-09-03 17:56:55 -0300

    docs(config): update version to v1.0.0-rc.1 and add beta version to the list

--- docsector.config.js
+++ docsector.config.js
@@ -8,11 +8,15 @@ export default {
   branding: {
     logo: 'https://docs.bootgly.com/images/logo/bootgly-logo.webp',
     name: 'Bootgly',
-    version: 'v1.0.0-beta.6',
+    version: 'v1.0.0-rc.1',
     versions: [
       {
-        id: 'v1.0.0-beta.6',
+        id: 'v1.0.0-rc.1',
         current: true,
+        released: true
+      },
+      {
+        id: 'v1.0.0-beta.6',
         released: true
       },
       {
